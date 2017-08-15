ac6c18ebd0d703a75dd1601f2655175a85c5704c
Debrena McEwen <debrenamcewen@gmail.com>
2017-08-15 10:32:59 -0700

    add changes to add function to get correct format for delete

--- tasks.js
+++ tasks.js
@@ -1,21 +1,24 @@
 #!/user/bin/env node
 const command = process.argv[ 2 ]
-const taskName = process.argv[ 3 ]
-const taskDescription = process.argv.slice([ 4 ]).join().replace(/\,/g, ' ')
+const taskName = process.argv.slice([ 3 ]).join().replace(/\,/g, ' ')
 const add = require('./commands/add')
 const list = require('./commands/list')
+const complete = require('./commands/complete')
 
 
 function commands ( command ) {
   switch (command) {
   case 'add':
-    add( taskName, taskDescription  )
+    add( taskName  )
     break;
 
   case 'list':
       list();
       break;
 
+  case 'complete':
+      complete( 'complete', taskName );
+      break;
   default:
     console.log('Invalid Entry');
   }
