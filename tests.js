3635c458a969a75a9b903a340f0e531337faa481
Debrena McEwen <debrenamcewen@gmail.com>
2017-08-16 14:14:59 -0700

    add test for complete function

--- tests.js
+++ tests.js
@@ -4,6 +4,7 @@ import chaiChange from 'chai-change'
 import allTasks from './allTasks.json'
 import tasks from './tasks.js'
 import add from './commands/add.js'
+import complete from './commands/complete.js'
 
 chai.use(chaiChange);
 
@@ -18,12 +19,20 @@ describe('tasks', () => {
     it( 'add a task object to allTasks',() => {
       add('test1')
       setTimeout( function () {
-        try {
           expect( allTasks.length ).to.equal( 1 );
-        } catch( error ) {
-          done( error );
-        }
       }, 100 );
     })
   })
+
+  context('complete()', () => {
+    it('sets a tasks complete property to true',() => {
+      complete(1)
+      expect(allTasks[0].complete).to.equal(true)
+    })
+  })
+
+
+
+
+
 })
