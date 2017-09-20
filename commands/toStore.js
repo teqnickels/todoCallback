5fdfc2f3b1d7898452176318d15896c2a93e67dc
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-20 12:10:37 -0700

    removes allTasks from being tracked, updates toStore

--- commands/toStore.js
+++ commands/toStore.js
@@ -2,11 +2,11 @@ const storage = require('../allTasks.json')
 var fs = require('fs')
 var complete = require('./complete')
 
-function toStore( obj ) {
+function toStore(obj) {
   var arrOfObj = (storage.length == 0 ? [] : storage)
-  arrOfObj.push( obj )
-  var data = JSON.stringify( arrOfObj )
-  fs.writeFile( './allTasks.json', data )
+  arrOfObj.push(obj)
+  var data = JSON.stringify(arrOfObj)
+  fs.writeFileSync('./allTasks.json', data)
   console.log(`You have ${arrOfObj.length++} tasks`)
 }
 
