732f0401f96a62937679fedc6c9ae0455dfaf760
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-22 09:59:58 -0700

    Add complete task feature

--- commands/complete.js
+++ commands/complete.js
@@ -1,18 +1,29 @@
 #!/user/bin/env node
 const toStore = require('./toStore')
-// const storage = require('../allTasks.json')
 var fs = require('fs')
 
 
-function complete(taskName) {
-  for (var i = 0; i < storage.length; i++) {
-    if (storage[i].id == taskName) {
-      storage[i].complete = true
-      var updateObj = storage
+function complete(filter) {
+  try {
+    const storage = require('../allTasks.json')
+    for (var i = 0; i < storage.length; i++) {
+      if (storage[i].id == filter) {
+        var description = storage[i].task
+        storage[i].complete = true
+        var updateObj = storage
+      }
     }
+    var update = JSON.stringify( updateObj )
+    fs.writeFile( './allTasks.json', update, (err) => {
+      if (err) throw (err)
+      console.log(`Completed task ${filter}: ${description}`)
+    })
+  }catch(err) {
+    if(err) {
+      console.log("Nothing in storage to 'complete'" )
+      return (err)
+    }
+
   }
-  var update = JSON.stringify( updateObj )
-  fs.writeFile( './allTasks.json', update )
-  console.log(`You have ${storage.length++} tasks`)
 }
 module.exports = complete;
