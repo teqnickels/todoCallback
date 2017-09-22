a0e708b746e37d1a227b26a46b394a0a7f9df8a2
Debrena McEwen <debrenamcewen@gmail.com>
2017-09-22 10:30:56 -0700

    add error handling to list function

--- commands/list.js
+++ commands/list.js
@@ -1,24 +1,30 @@
-// const storage = require('../allTasks.json')
 require('console.table');
 
 function list() {
-  var todoArr = []
-  var finalArr = []
-  for (var i = 0; i < storage.length; i++) {
-    var keys = Object.keys(storage[i])
-    if (storage[i].complete == false) {
-      var todos = storage[i].task
-      todoArr.push(todos)
+  try {
+    const storage = require('../allTasks.json')
+    var todoArr = []
+    var finalArr = []
+    for (var i = 0; i < storage.length; i++) {
+      var keys = Object.keys(storage[i])
+      if (storage[i].complete == false) {
+        var todos = storage[i].task
+        todoArr.push(todos)
+      }
+    }
+    todoArr.forEach(function (element, index) {
+      var todoObj = {}
+      todoObj.id = index + 1
+      todoObj.description = element
+      finalArr.push( todoObj )
+    })
+    console.table(finalArr)
+    console.log(`You have ${ todoArr.length } tasks`);
+  } catch(err) {
+    if(err) {
+      console.log('There are no tasks to list')
     }
   }
-  todoArr.forEach(function (element, index) {
-    var todoObj = {}
-    todoObj.id = index + 1
-    todoObj.description = element
-    finalArr.push( todoObj )
-  })
-  console.table(finalArr)
-  console.log(`You have ${ todoArr.length } tasks`);
 }
 
 
